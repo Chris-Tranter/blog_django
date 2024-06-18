e4a5988526c9c0543653bf63a493855c87412bbe
Christopher Tranter <mr_crock@hotmail.co.uk>
2024-06-18 14:36:56 +0000

    created secret key and linked database

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+%1mh!xaklxsa#f!p=i*)6p4wzovu1bgl#fal4u3*_-_t%9swd'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
